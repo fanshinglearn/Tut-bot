3538edef65aef88ef7c814e66408a656a9ddb453
fanshinglearn <fanshinglearn@gmail.com>
2024-03-15 19:42:01 +0800

    Create 筆記.py

---
+++ 筆記.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+# intents default 或 all 或 none
+intents = discord.Intents.default()
+
+# 打該 members 的權限
+intents.members = True
+
+# command_prefix 命令前綴
+bot = commands.Bot(command_prefix='[', intents=intents)
+
+# 啟動
+@bot.event
+async def on_ready():
+    print('>> bot is online <<')
+
+# 成員加入
+@bot.event
+async def on_member_join(members):
+    channel = bot.get_channel(1217763083288580136)
+    await channel.send(f'{members} join!')
+
+# 成員離開
+@bot.event
+async def on_member_remove(members):
+    channel = bot.get_channel(1217763163378810890)
+    await channel.send(f'{members} leave!')
+
+# 啟動 bot
+bot.run('MTE4ODExNDg3NDExMTM2MTEwNQ.GTJgOR.uP8-f688QG_z5XSVwHxttI3zPokl3kacLaweLg')
