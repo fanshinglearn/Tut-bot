dc4594a3db6e7bfc49b73766c5d1eb780124fd3e
fanshinglearn <fanshinglearn@gmail.com>
2024-03-15 21:26:07 +0800

    【Proladon】Code a discord bot - EP4 - Setting.json 設定檔

---
+++ EP4.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import json
+
+with open('./setting.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='[', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('>> bot is online <<')
+
+@bot.event
+async def on_member_join(members):
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
+    await channel.send(f'{members} join!')
+
+@bot.event
+async def on_member_remove(members):
+    channel = bot.get_channel(int(jdata['Leave_channel']))
+    await channel.send(f'{members} leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+bot.run(jdata['TOKEN'])
