bb77cde7b9d61fcc7b14c8cf0367823ef54a3b29
fanshinglearn <fanshinglearn@gmail.com>
2024-03-15 20:20:12 +0800

    Update 筆記 EP3

--- 筆記.py
+++ 筆記.py
@@ -2,10 +2,12 @@ import discord
 from discord.ext import commands
 
 # intents default 或 all 或 none
-intents = discord.Intents.default()
+# 要打指令需要 ? 權限
+intents = discord.Intents.all()
+# intents = discord.Intents.default()
 
 # 打該 members 的權限
-intents.members = True
+# intents.members = True
 
 # command_prefix 命令前綴
 bot = commands.Bot(command_prefix='[', intents=intents)
@@ -26,6 +28,11 @@ async def on_member_join(members):
 async def on_member_remove(members):
     channel = bot.get_channel(1217763163378810890)
     await channel.send(f'{members} leave!')
+
+# 顯示機器人延遲 指令
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
 
 # 啟動 bot
 bot.run('MTE4ODExNDg3NDExMTM2MTEwNQ.GTJgOR.uP8-f688QG_z5XSVwHxttI3zPokl3kacLaweLg')
