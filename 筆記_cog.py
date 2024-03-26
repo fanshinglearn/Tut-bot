6fa6653f59cea2af84a5ab61dc83c71775edc459
fanshinglearn <fanshinglearn@gmail.com>
2024-03-27 01:27:48 +0800

    Update 筆記_cog.py

--- 筆記_cog.py
+++ 筆記_cog.py
@@ -1,6 +1,7 @@
 # < ---------- cmds ---------->
 import discord
 from discord.ext import commands
+import datetime
 
 # 繼承 core.classes.py 的 __init__
 from core.classes import Cog_Extension
@@ -25,6 +26,21 @@ class Main(Cog_Extension):
         # if msg.content == 'apple' and msg.author != self.bot.user:
         if msg.content in keyword and msg.author != self.bot.user:
             await msg.channel.send('apple')
+    
+    # Embed 嵌入訊息
+    @commands.command()
+    async def em(self, ctx):
+        embed=discord.Embed(title="op.gg", url="https://www.op.gg/", description="lol", color=0x4596bf,
+                            # 時間截
+                            timestamp=datetime.datetime.now())
+        embed.set_author(name="fanshing", icon_url="https://lh3.googleusercontent.com/-JfyMf7Wf_tboP30yAhPOxFUmULHpj55RGXVoSHpSzMoVJIBYtAntWmmkpu37dpYFSP6Dm6FwJoY7dGwQ8Ec3zEgvu8ZAO-H9nbbIAlt81oD1Q4tOMfU=h60")
+        embed.set_thumbnail(url="https://lh3.googleusercontent.com/-JfyMf7Wf_tboP30yAhPOxFUmULHpj55RGXVoSHpSzMoVJIBYtAntWmmkpu37dpYFSP6Dm6FwJoY7dGwQ8Ec3zEgvu8ZAO-H9nbbIAlt81oD1Q4tOMfU=h60")
+        embed.add_field(name="中飛妮可", value="隨機單中", inline=False)
+        embed.add_field(name="2", value="222", inline=False)
+        embed.add_field(name="3", value="333", inline=False)
+        embed.add_field(name="4", value="444", inline=False)
+        embed.set_footer(text="嬰兒")
+        await ctx.send(embed=embed)
 
 
 # 主程式呼叫 setup 傳入 bot 給 Main class
